8fdd28e502c5c1e11737f8869f8651650c6139b1
ovie4louis <enakarhireovie7@gmail.com>
2024-10-16 15:30:08 +0100

    task

---
+++ Dictionaries.py
@@ -0,0 +1,14 @@
+
+band = {
+    "vocals" : "plant",
+    "guitar" : "page"
+
+}
+
+band2 = dict(vocals = "plant", guitar = "page")
+
+print(band)
+print(band2)
+
+print(type(band))
+print(len(band))
